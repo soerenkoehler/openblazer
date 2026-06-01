5c24431b9f2fd3bd3e9f29b4d620c8b910737867
Sören Köhler <soeren.koehler@enbw.com>
2026-06-01 21:52:22 +0200

    restructure project

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,9 +3,11 @@
 main() {
     THIS_SCRIPT=$(readlink -e "$0")
     DIR_THIS_SCRIPT=$(dirname "$THIS_SCRIPT")
+    DIR_GAME=$(readlink -e "./game")
+    DIR_DIST=$(readlink -e "./dist")
 
     PROJECT=$(
-        grep -E '^config/name=".+"' "./game/project.godot" \
+        grep -E '^config/name=".+"' "$DIR_GAME/project.godot" \
         | cut -d'=' -f2 \
         | tr -d '"'
     )
@@ -42,13 +44,18 @@ initialize_workspace() {
         exit -1
     fi
 
-    # prepare output directories
-    DIR_DIST=$(readlink -f ./dist)
-
-    for DIR in $DIR_DIST; do
-        mkdir -v -p $DIR
-        chmod -v 777 $DIR
-    done
+    # set release mode
+    if [[ $GITHUB_REF_TYPE == 'tag' ]]; then
+        RELEASE="$GITHUB_REF_NAME"
+        if [[ $RELEASE =~ ^v([0-9]+\.[0-9]+\.[0-9]+) ]]; then
+            VERSION="${BASH_REMATCH[1]}"
+        else
+            printf "tag name %s does not begin with required semver 'vMAJOR.MINOR.PATCH'\n" "$GITHUB_REF_NAME"
+            exit -1
+        fi
+    elif [[ $GITHUB_REF_TYPE == 'branch' ]]; then
+        RELEASE=nightly
+    fi
 }
 
 install_godot() {
@@ -71,7 +78,12 @@ install_godot() {
 }
 
 package() {
-    ./godot --headless --path ./game --export-release "Windows Desktop" "$DIR_DIST/$PROJECT.exe"
+    # export Godot projects
+
+    ./godot --headless --path "$DIR_GAME" --export-release "Windows Desktop" "$DIR_DIST/$PROJECT.exe"
+    ./godot --headless --path "$DIR_GAME" --export-release "Linux"           "$DIR_DIST/$PROJECT.x64"
+
+    # create MSIX package
 
     cp "$DIR_DIST/$PROJECT.exe" "./msix/"
 
@@ -82,25 +94,40 @@ package() {
         pack \
         -d "./msix" \
         -p "./dist/$PROJECT.msix"
+    xsltproc \
+        --stringparam new-version "$VERSION" \
+        update-appmanifest.xslt \
+        AppxManifest.xml > AppxManifest.updated.xml
+
+    # create SHA256 hashes
+
+    pushd "$DIST_DIR"
+
+    rm *.sha256
+    for FILE in *
+    do
+        sha256sum "$FILE" >$FILE.sha256
+    done
+
+    popd
 }
 
 release() {
     printf "verify github auth status:\n%s\n\n" "$(gh auth status)"
 
-    if [[ $GITHUB_REF_TYPE == 'tag' ]]; then
+    # first (quick win): create and upload GitHub release
+    if [[ $RELEASE == nightly ]]; then
         create_release_prod
-    elif [[ $GITHUB_REF_TYPE == 'branch' ]]; then
+    else
         create_release_nightly
     fi
 
-    if [[ -e $DIR_DIST ]]; then
-        upload_artifacts
-        # TODO if [[ $RELEASE != nightly ]]; then
-            publish_to_msstore
-        # fi
-   else
-        printf "no artifacts to upload\n"
-    fi
+    upload_artifacts
+
+    # second (long duration): publish to MS Store
+    # FIXME if [[ $RELEASE != nightly ]]; then
+        # publish_to_msstore
+    # fi
 }
 
 create_release_prod() {
@@ -192,7 +219,7 @@ publish_to_msstore() {
     printf "uploading artifacts to MS Store\n"
 
     msstore publish \
-        "$DIR_DIST/openblazer.msix" \
+        "./$PROJECT.msix" \
         --verbose \
         --appId "$MSSTORE_STORE_ID"
 }
